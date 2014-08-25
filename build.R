c19946feca51c7e44e4ac85e0975e1f5801e8a08
Tom <thomas@schwarzl.net>
2014-08-25 09:10:19 +0200

    sorted data

--- build.R
+++ build.R
@@ -2,7 +2,7 @@ detach("package:limmas")
 remove.packages("limmas")
 library(devtools)
 setwd("~/bioc/limmas")
-build("limmas", binary=T)
-install.packages("limmas", repos=NULL)
-
+build("limmas", binary = T)
+#install.packages("limmas")
+# R CMD install limmas_0.3.tar.gz
 #promptPackage("limmas") 
