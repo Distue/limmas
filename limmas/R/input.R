ffea2a61f234114fbe288c39b0c7d5acac138cf4
Tom <thomas@schwarzl.net>
2015-09-02 18:53:06 +0200

    added roxify tags

--- limmas/R/input.R
+++ limmas/R/input.R
@@ -5,14 +5,29 @@
 # ------------------------------------------
 # public functions
 # ------------------------------------------
-
+##' Reads in a pheno file text file.
+##'  
+##' @title read.pheno
+##' @param file fileame
+##' @param sampleNamesCol column describing the sample names
+##' @param originalNamesCol column describing the original names
+##' @return SmartAnnotatedDataFrame pheno information
+##' @export
 read.pheno <- function(file, sampleNamesCol="", originalNamesCol="intensityCols", header=T, row.names=1, sep="\t", ...) {
-   pheno <- read.delim(file, header=T, row.names=1, ...)
+   pheno <- read.delim(file, header=header, row.names=row.names, sep=sep, ...)
    rownames(pheno) <- gsub(" ", ".", rownames(pheno))
    return(SmartAnnotatedDataFrame(originalNamesCol=originalNamesCol, sampleNamesCol=sampleNamesCol, pheno))
 }
 
-
+##' Read in a maxQuant proteinGroups file
+##'  
+##' @title read.maxQuant
+##' @param file fileame
+##' @param pheno pheno information 
+##' @param idcolumn name of column containing the protein id
+##' @param splitIds locial if the ids should be split 
+##' @return ExpressionSet protein intensities and annotation as ExpressionSet
+##' @export
 read.maxQuant <- function(file, pheno, idcolumn="Protein.IDs", splitIds = F, ...) {
    if(!is.character(file)) {
       stop("file is not a character string")
@@ -41,6 +56,15 @@ read.maxQuant <- function(file, pheno, idcolumn="Protein.IDs", splitIds = F, ...
 }
 
 
+##' Creates an ExpressionSet from the proteinGroups data frame
+##'  
+##' @title createExpressionSetFromMaxQuant
+##' @param file fileame
+##' @param pheno pheno information 
+##' @param idcolumn name of column containing the protein id
+##' @param annotation feature annotations
+##' @return ExpressionSet protein intensities and annotation as ExpressionSet
+##' @export
 createExpressionSetFromMaxQuant <- function(data, pheno, idcolumn="Protein.IDs", annotation=as.character(data[,idcolumn]), ...) {
    if(!is.data.frame(data)) {
       stop("data is not a data frame.")
@@ -67,8 +91,15 @@ createExpressionSetFromMaxQuant <- function(data, pheno, idcolumn="Protein.IDs",
    return(createExpressionSet(as.matrix(intensities), pheno, features, annotation))
 }
 
-
-# create an expression set expression set and annotated data frame:
+##' Create an expression set expression set and annotated data frame
+##'  
+##' @title createExpressionSet
+##' @param intensities intensity matrix
+##' @param pheno pheno information 
+##' @param features feature meta data
+##' @param annotation feature annotations
+##' @return ExpressionSet protein intensities and annotation as ExpressionSet
+##' @export
 createExpressionSet <- function(intensities, pheno, features, annotation, ...) {
    if(!(is(pheno, "SmartAnnotatedDataFrame") || is(pheno, "AnnotatedDataFrame"))) {
       stop("pheno is not an object of class AnnotatedDataFrame or SmartAnnotatedDataFrame.")
@@ -86,7 +117,13 @@ createExpressionSet <- function(intensities, pheno, features, annotation, ...) {
       stop("intensities is not a matrix or a data frame.")
    }
    if(is(pheno, "SmartAnnotatedDataFrame")) {
-       pheno <- getAnnotatedDataFrame(pheno)
+      pheno <- getAnnotatedDataFrame(pheno)
    }
    return(new("ExpressionSet", exprs=intensities, phenoData=pheno, featureData=features, annotation=annotation, ...))
 }
+
+
+# ------------------------------------------
+# private functions
+# ------------------------------------------
+
